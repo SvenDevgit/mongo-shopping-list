9fb51bd37765c6ddec29514e703e2e0490acb3cd
SvenDevgit <svenlambeck@hotmail.com>
2016-08-08 19:59:54 +0200

    commit

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
+                            'mongodb://MongoUser01:MongoUser01@ds029665.mlab.com:29665/nodecourse' :
                             'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
